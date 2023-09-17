10c8c4c844c2b759d48b71eda265ba809ab061d9
Manassarn Manoonchai <manassarn@gmail.com>
2023-09-17 23:43:01 +0700

    Highlight items

--- src/lib/components/Recipe.svelte
+++ src/lib/components/Recipe.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
 	import { ingredients, type Recipe } from '$lib/data';
+	import { ingredientsStore } from '$lib/store';
 
 	export let key: string;
 	export let recipe: Recipe;
+
+	$: ingredientsRequirements = recipe.ingredients
+		.map((ingredientReq) => {
+			const ingredient = ingredients[ingredientReq.key];
+
+			return {
+				name: ingredient.name,
+				key: ingredientReq.key,
+				quantity: ingredientReq.quantity,
+				owned: $ingredientsStore[ingredientReq.key] || 0
+			};
+		})
+		.map((ingredient) => {
+			// Calculation
+			return {
+				...ingredient,
+				needMore: Math.max(ingredient.quantity - ingredient.owned, 0)
+			};
+		});
 </script>
 
 <div class="card">
@@ -11,11 +31,14 @@
 		{recipe.name}
 	</header>
 
-	<section class="p-4 flex flex-row space-x-2">
-		<div class="flex flex-col space-y-2">
-			{#each recipe.ingredients as ingredientReq}
-				{@const ingredient = ingredients[ingredientReq.key]}
-				<div class="flex flex-row space-x-2">
+	<section class="p-4 flex flex-row space-x-2 justify-center">
+		<div class="flex flex-col space-y-2 w-full">
+			{#each ingredientsRequirements as ingredient}
+				<div
+					class="badge"
+					class:variant-ghost-surface={ingredient.needMore > 0 && ingredient.owned > 0}
+					class:variant-ghost-success={ingredient.needMore == 0}
+				>
 					<div class="flex flex-col space-y-2">
 						<div>{ingredient.name}</div>
 					</div>
@@ -23,11 +46,16 @@
 						<img
 							height={32}
 							width={32}
-							src={`/images/ingredients/${ingredientReq.key}.png`}
+							src={`/images/ingredients/${ingredient.key}.png`}
 							alt={ingredient.name}
 						/>
 					</div>
-					x {ingredientReq.quantity}
+					x {ingredient.quantity}
+					{#if ingredient.needMore > 0 && ingredient.owned > 0}
+						<div class="flex flex-col space-y-2">
+							<div class="text-red-500">({ingredient.needMore})</div>
+						</div>
+					{/if}
 				</div>
 			{/each}
 		</div>
